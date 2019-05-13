900aa5986183e07ebdf94aa92764415628bf3225
Xinci0n <caballerofermi@gmail.com>
2019-05-13 07:01:05 -0500

    about concept

--- resources/js/pages/AboutUs/Index.vue
+++ resources/js/pages/AboutUs/Index.vue
@@ -1,16 +1,12 @@
 <template>
-    <div>
-        <b-container class="bv-example-row">
-            <b-row>
-                <b-col>1 of 3
-                    Investors
-                </b-col>
-                <b-col>2 of 3</b-col>
-                <b-col>3 of 3</b-col>
-            </b-row>
-        </b-container>
-    </div>
-    
+     <div class="space">
+     Our mission is to create an global economic system from the ground up, in the direction of an alternative, post-capitalist model, and thus open the way for collective change towards a common life. The development and use of powerful interconnected (global) digital tools and regional (local) networks is crucial to our success. We are using our time and energy constructively and collaboratively to create real alternatives; we are putting theories into practice.
+  <div class="land">
+    <img class="img1" src="https://i.imgsafe.org/fb/fb7296bc04.jpeg">
+    <img class="img3" src="https://i.imgsafe.org/fc/fc1c77d1ae.jpeg">
+    <img class="img2" src="https://i.imgsafe.org/fc/fc2cc1b828.png"> 
+  </div>
+  </div>
 </template>
 
 <script>
@@ -20,5 +16,28 @@ export default {
 </script>
 
 <style lang="scss">
-
+.img1 {
+  position:absolute;
+  border-radius: 15px 50px 30px 5px;
+  top:300px;
+  left:50px;
+  width:500px;
+  height:430px;
+}
+.img2 {
+  top:450px;
+  left:400px;
+  position:absolute;
+  border-radius: 15px 50px 30px 5px;
+  width:530px;
+  height:450px;
+}
+.img3 {
+  position:absolute;
+  border-radius: 15px 50px 30px 5px;
+  top:580px;
+  left:820px;
+  width:520px;
+  height:450px;
+}
 </style>
